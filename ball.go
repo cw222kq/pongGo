0123c876dd6e52efedc242cb44a469cb7a904482
cw222kq <lotten_w@hotmail.com>
2024-11-04 13:17:06 +0100

    Add ball and bouncing effect

---
+++ ball.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	rl "github.com/gen2brain/raylib-go/raylib"
+)
+
+type ball struct {
+	position rl.Vector2
+	speed    rl.Vector2
+	radius   float32
+}
+
+func newBall(posX float32, posY float32, speedX float32, speedY float32) *ball {
+	return &ball{
+		position: rl.Vector2{X: posX, Y: posY},
+		speed:    rl.Vector2{X: speedX, Y: speedY},
+		radius:   10.0,
+	}
+}
+
+func (b *ball) spawn(fadeValue int) {
+	rl.DrawCircleV((*b).position, float32((*b).radius), (rl.NewColor(255, 255, 255, byte(fadeValue))))
+}
+
+func (b *ball) move(screenWitdh float32, screenHeight float32) { // - left & - up
+	b.position.X += b.speed.X
+	b.position.Y += b.speed.Y
+
+	minX := b.radius
+	minY := b.radius
+	maxX := float32(screenWitdh) - b.radius
+	maxY := float32(screenHeight) - b.radius
+
+	if minX > b.position.X || maxX < b.position.X {
+		b.speed.X = -b.speed.X
+	}
+
+	if minY > b.position.Y || maxY < b.position.Y {
+		b.speed.Y = -b.speed.Y
+	}
+}
--- main.go
+++ main.go
@@ -5,27 +5,52 @@ import (
 )
 
 func main() {
-	screenWitdh := int32(1280)
-	screenHeight := int32(720)
+	screenWitdh := int32(1600)
+	screenHeight := int32(900)
 
-	rl.InitWindow(screenWitdh, screenHeight, "raylib [core] example - basic window")
+	rl.InitWindow(screenWitdh, screenHeight, "PONG")
 	//rl.SetWindowState(rl.FlagWindowResizable)
 	defer rl.CloseWindow()
 
 	rl.SetTargetFPS(60)
 
-	for !rl.WindowShouldClose() {
+	//startTime := time.Now()
+
+	var b *ball
+
+	fadeOutStartText := 255
+	fadeInGame := 0
+
+	for !rl.WindowShouldClose() { // monitorWidth 1920, monitorHeight 1080
+
+		//elapsedTime := time.Since(startTime).Seconds()
+
 		rl.BeginDrawing()
 
-		text := "Congrats! You created your first window!"
-		textSize := int32(20)
+		if fadeOutStartText > 0 {
+			fadeOutStartText -= 3
+		}
+
+		if fadeOutStartText == 0 {
+			if fadeInGame < 255 {
+				fadeInGame += 15
+			}
+			if b == nil {
+				b = newBall(float32(screenWitdh/2), float32(screenHeight/2), 3, 3)
+			} else {
+				b.spawn(fadeInGame)
+				b.move(float32(screenWitdh), float32(screenHeight))
+			}
+		}
+
+		rl.ClearBackground(rl.Black)
+		text := "Welcome to Pong!"
+		textSize := int32(30)
 		textWidth := int32(rl.MeasureText(text, textSize))
 
 		x := (screenWitdh - textWidth) / 2
-		y := screenHeight / 2
-
-		rl.ClearBackground(rl.RayWhite)
-		rl.DrawText(text, x, y, textSize, rl.LightGray)
+		y := (screenHeight - textSize) / 2
+		rl.DrawText(text, x, y, textSize, rl.NewColor(255, 255, 255, byte(fadeOutStartText)))
 
 		rl.EndDrawing()
 	}
